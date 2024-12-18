c977326550d3d4030a33f02cb23dd62ae193b955
дмитрий Madara <madara@deeplay.io>
2024-12-18 12:43:43 +0600

    Update packages

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup, find_packages
 
 
+def requires(filename: str):
+    return open(filename).read().splitlines()
+
+
 if __name__ == '__main__':
     setup(
         name='mm-tools',
         packages=find_packages(),
-        install_requires=[
-            'aiosqlite',
-            'peewee',
-            'peewee-async',
-            'aiopg',
-            'peewee-moves',
-        ]
+        install_requires=requires("requirements.txt"),
     )
