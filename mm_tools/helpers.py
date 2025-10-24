eefbeb8bf860e6d6c24825f2187e7df83879cea6
Sadisms <dmitriy.stepanenko.2011@mail.ru>
2025-10-24 16:54:12 +0600

    fix(helpers): adjust compression preset in compress_json function for improved performance

--- mm_tools/helpers.py
+++ mm_tools/helpers.py
@@ -5,7 +5,7 @@ import msgpack
 
 def compress_json(data: dict) -> str:
     packed = msgpack.packb(data, use_bin_type=True)
-    compressed = lzma.compress(packed, preset=22)
+    compressed = lzma.compress(packed, preset=9)
     return base64.b85encode(compressed).decode('ascii')
 
 
@@ -13,4 +13,3 @@ def decompress_json(compressed_str: str) -> dict:
     compressed = base64.b85decode(compressed_str.encode('ascii'))
     packed = lzma.decompress(compressed)
     return msgpack.unpackb(packed, raw=False)
-
